d0b57cfe0cf821e840b5c6041456301909a6b702
y.cui <y.cui@controlexpert.com>
2016-02-26 16:17:59 +0800

    Removed useless usings.

--- Cano.ToolSet/Serializer.cs
+++ Cano.ToolSet/Serializer.cs
@@ -1,13 +1,7 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace Cano.ToolSet
