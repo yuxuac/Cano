92503580bc18cb1bac704cc1af15d3f2f47cde33
y.cui <y.cui@controlexpert.com>
2016-02-26 18:29:07 +0800

    Added
    1. Const.cs
    2. IO.cs

---
+++ Cano.ToolSet/Const.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cano
+{
+    /// <summary>
+    /// Consts
+    /// </summary>
+    public static class Const
+    {
+        /// <summary>
+        /// 得到当前时间格式化字符串
+        /// </summary>
+        public static DT DateTime = new DT();
+    }
+
+    /// <summary>
+    /// 当前时间格式化字符串
+    /// </summary>
+    public class DT
+    {
+        /// <summary>
+        /// 返回日期格式: yyyyMMddHHmmss
+        /// </summary>
+        public string Format1
+        {
+            get
+            {
+                return DateTime.Now.ToString("yyyyMMddHHmmss");
+            }
+            private set { }
+        }
+
+        /// <summary>
+        /// 返回日期格式: yyyyMMdd-HHmmss
+        /// </summary>
+        public string Format2
+        {
+            get
+            {
+                return DateTime.Now.ToString("yyyyMMdd-HHmmss");
+            }
+            private set { }
+        }
+
+        /// <summary>
+        /// 返回日期格式: yyyy-MM-dd HH:mm:ss
+        /// </summary>
+        public string Format3
+        {
+            get
+            {
+                return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            private set { }
+        }
+
+        /// <summary>
+        /// 返回日期格式: yyyy-MM-dd HH:mm:ss ffff
+        /// </summary>
+        public string Format4
+        {
+            get
+            {
+                return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+            }
+            private set { }
+        }
+    }
+}
---
+++ Cano.ToolSet/IO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cano
+{
+    public class IO
+    {
+        public static void WritePlainText(string content, string file, bool append = false)
+        {
+            using (StreamWriter sw = new StreamWriter(file, append))
+            {
+                sw.WriteLine(content);
+            }
+        }
+
+        public static void WritePlainText(string content, string file, Encoding encoding, bool append = false)
+        {
+            using (StreamWriter sw = new StreamWriter(file, append, encoding))
+            {
+                sw.WriteLine(content);
+            }
+        }
+
+        public static string ReadPlainText(string file)
+        {
+            using (StreamReader sr = new StreamReader(file))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        public static string ReadPlainText(string file, Encoding encoding)
+        {
+            using (StreamReader sr = new StreamReader(file, encoding))
+            { 
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
